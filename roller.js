04ae691ae92ca8846b70df20223cbc5b881be1a1
gr1mlock16 <gr1mlock_16@outlook.com>
2021-04-29 15:59:54 -0600

    test1

---
+++ roller.js
@@ -0,0 +1,18 @@
+const results = document.getElementById("rolled")
+
+
+const roll = () => {
+    let input = document.getElementById("multidice").value;
+    let number =Math.floor((Math.random() * 6) + 1); 
+    let numbers = input*number;
+    results.innerHTML= numbers;
+    console.log(input);
+}
+
+
+
+
+
+document.getElementById("dice").addEventListener('click',() =>roll())
+
+    
